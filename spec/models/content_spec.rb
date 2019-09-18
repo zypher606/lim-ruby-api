e15d70668cfebc80f85f7474b316add3f0c3b1cc
zypher606 <ashimrajkonwar@gmail.com>
2019-09-18 18:08:42 +0530

    Final touch repo

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Content, type: :model do
   # Association test
-  # ensure an item record belongs to a single webpage record
+  # ensure a content record belongs to a single webpage record
   it { should belong_to(:webpage) }
   # Validation test
   # ensure column content_type and content_value are present before saving
--- spec/models/webpage_spec.rb
+++ spec/models/webpage_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Webpage, type: :model do
   # Association test
-  # ensure Todo model has a 1:m relationship with the Content model
+  # ensure Webpage model has a 1:m relationship with the Content model
   it { should have_many(:contents).dependent(:destroy) }
   # Validation tests
   # ensure column url is present before saving
